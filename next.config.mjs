9405e6478d45d814bf314f9b195653a9ac7e9c2a
Olivier Buitelaar <37653836+ollieb89@users.noreply.github.com>
2025-02-16 20:07:35 +0000

    fixes in deployment scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true
-  }
+  },
+  trailingSlash: true,
+  basePath: '',
 };
 
 export default nextConfig;
