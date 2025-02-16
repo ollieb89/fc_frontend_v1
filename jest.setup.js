ff44d670eff7b77e63d2e6809b7efafe5979f317
Olivier Buitelaar <37653836+ollieb89@users.noreply.github.com>
2025-02-16 19:04:02 +0000

    version fix

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+
+// Mock Request object for Next.js tests
+global.Request = class Request {
+  constructor(input, init) {
+    this.input = input;
+    this.init = init;
+  }
+};
